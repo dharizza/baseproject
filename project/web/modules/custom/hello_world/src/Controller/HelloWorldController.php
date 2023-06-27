40631554b64bdb36fd28c9498acd551493a09c95
Dharizza Espinach B <dharizza@gmail.com>
2023-06-27 01:37:13 +0000

    Added helloWorld controller and first route.

---
+++ project/web/modules/custom/hello_world/src/Controller/HelloWorldController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Drupal\hello_world\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Returns responses for Hello World routes.
+ */
+class HelloWorldController extends ControllerBase {
+
+  /**
+   * Builds the response.
+   */
+  public function build() {
+    $build['content'] = [
+      '#type' => 'item',
+      '#markup' => $this->t('Hello World!'),
+    ];
+
+    return $build;
+  }
+
+}
