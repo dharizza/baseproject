4900f962b13286e5c33d864048a9758130e146c2
Dharizza Espinach B <dharizza@gmail.com>
2023-06-27 01:39:27 +0000

    Use current_user service.

--- project/web/modules/custom/hello_world/src/Controller/HelloWorldController.php
+++ project/web/modules/custom/hello_world/src/Controller/HelloWorldController.php
@@ -13,9 +13,18 @@ class HelloWorldController extends ControllerBase {
    * Builds the response.
    */
   public function build() {
+    $user = \Drupal::currentUser();
+
+    if (!$user->isAnonymous()) {
+      $name = $user->getDisplayName();
+    }
+    else {
+      $name = "World";
+    }
+
     $build['content'] = [
       '#type' => 'item',
-      '#markup' => $this->t('Hello World!'),
+      '#markup' => $this->t('Hello @name!', ['@name' => $name]),
     ];
 
     return $build;
